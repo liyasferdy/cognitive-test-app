293c7d6c6a95a5e9e92c19819ce5a88ed94b83f4
liyasferdy <liyastarigan011@gmail.com>
2024-12-18 11:22:24 +0700

    fix-error:button

--- src/app/test-mm/instruction/page.jsx
+++ src/app/test-mm/instruction/page.jsx
@@ -31,7 +31,6 @@ export default function InstructionMM() {
 
           {/* Kontainer untuk tombol */}
           <div className="flex justify-center items-center bottom-5 left-0 right-0 mt-5 mb-2 px-5 sm:px-20 z-50">
-            return (
             <Button
               color="primary"
               size="lg"
