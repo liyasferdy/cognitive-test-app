c8ff7985c102afbb2f5bf000b9697a16a80da6c2
liyasferdy <liyastarigan011@gmail.com>
2024-12-18 11:23:55 +0700

    fix-error:button

--- src/app/test-mm/instruction/page.jsx
+++ src/app/test-mm/instruction/page.jsx
@@ -15,8 +15,8 @@ export default function InstructionMM() {
 
   return (
     <AuthWrapper>
-      <div className="pt-20 pb-20 sm:pt-40 flex justify-center mt-10 sm:mt-40 items-center p-5 relative">
-        <Card className="w-full sm:max-w-[60rem] px-5 sm:px-20 py-10 relative sm:bottom-40 min-h-[400px]">
+      <div className="pt-20 pb-20 sm:pt-40 flex justify-center items-center mt-10 sm:mt-40 p-5 relative">
+        <Card className="w-full sm:max-w-[60rem] px-5 sm:px-20 py-10 min-h-[400px]">
           <CardBody>
             <h1 className="text-slate-900 text-lg sm:text-xl text-center font-semibold">
               Instruksi - Test MM
@@ -30,11 +30,11 @@ export default function InstructionMM() {
           </CardBody>
 
           {/* Kontainer untuk tombol */}
-          <div className="flex justify-center items-center bottom-5 left-0 right-0 mt-5 mb-2 px-5 sm:px-20 z-50">
+          <div className="fixed bottom-5 left-0 right-0 flex justify-center items-center px-5 sm:px-20 z-50">
             <Button
               color="primary"
               size="lg"
-              className="w-full sm:w-auto"
+              className="w-full max-w-xs sm:w-auto sm:max-w-none"
               onClick={handleNavigation}
             >
               Mulai Test
